6b5fc39773843e66b2501bed1c3667312f8d98b5
Diego <diego@MacBook-Pro-de-Diego.local>
2017-04-22 11:40:44 -0500

    Dojo angular 2

---
+++ Angular/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BookService } from '../../services/book.service';
+
+@Component({
+  selector: 'my-books',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookComponent implements OnInit {
+  heading = 'Library';
+  cash = 10000;
+  books: Book[];
+
+  constructor(private bookService: BookService) {
+
+  }
+
+  ngOnInit(): void {
+    this.bookService.getBooks()
+      .then(books => this.books = books.slice(0, 5));
+  }
+
+  totalCost() {
+
+    let sum = 0;
+    if(this.books){
+      for(let book of this.books){
+        if(!book.isAvailable){
+          sum += book.price;
+        }
+      }
+    }
+    return sum;
+  }
+
+  castDate(date) {
+    return new Date(date);
+  }
+
+  cashLeft() {
+  //Dinero restante de la compra de los libros
+  return this.cash-this.totalCost();
+  }
+
+  buy(book) {
+    if (this.cashLeft() >= book.price) {
+      book.isAvailable = false;
+    } else {
+      alert("You don't have enough cash");
+    }
+  }
+
+  cancelBuy(book) {
+    book.isAvailable = true;
+  }
+
+}
